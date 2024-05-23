691d7b12266323c8723bfac274155372cb4e57e7
Ali Grich <128514098+grichali@users.noreply.github.com>
2024-05-23 17:36:38 +0100

    add entity

--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c8e31e41d1e411a1bd56e21eeaacb530b75035")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91bbbc0ba9e0f2340748cc958ca01af8bf0ecdb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
