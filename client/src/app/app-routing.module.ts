e3cbe843b6c660b52d75ce443cf85fc35abe7778
Ali Grich <128514098+grichali@users.noreply.github.com>
2024-08-08 18:12:32 +0100

    Commit before Angular update

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ROUTES } from '@angular/router';
+
+routes: ROUTES
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
