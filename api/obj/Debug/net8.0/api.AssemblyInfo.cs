0753e8cc3c276973efff1654992aafd2704a1def
Ali Grich <128514098+grichali@users.noreply.github.com>
2024-08-18 15:58:27 +0100

    update signin with google

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d84081be6ea8ca254b1c9d518c81faaa46fb3602")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5905db36342c58d159af3cb24a212b76a44c3038")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
