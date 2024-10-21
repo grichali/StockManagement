0b8627089e24ae6bce4d0146d99899be2d3f8388
youssef belkhiri <belkhiriyoussef33@gmail.com>
2024-10-21 11:31:12 +0100

    signup page

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+240f95a30fce55b16943dabc304dc73e016e5015")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0139c06b69a17a998e56327568bd0d9cdc1ca60e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
